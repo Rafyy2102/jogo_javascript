d0a941972989c4658fae9f0fde522e03946f56bb
Rafyy2102 <rafpromais@yahoo.com.br>
2020-06-12 17:17:08 -0300

    criando a comida da cobrinha

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 //trazenddo o canvas para o javascript
 let canvas = document.getElementById("snake");
+
 //trabalhando em 2d mostranddo o campo do jogo
 let context = canvas.getContext("2d");
+
 //tamanho do campo do jogo
 let box = 32;
+
 //array da cobrinha
 let snake = [];
-//dentro da cobrinha
+
+//tamanho dentro da cobrinha
 snake [0] = {
     y: 8 * box,
     x: 8 * box
@@ -15,12 +19,19 @@ snake [0] = {
 //direção da cobrinha
 let direction = "right";
 
+//criando array para a comida
+let food = {
+    //Math.floor tira a flutação do math e randomm traz números aleatórios
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
+
 //criando o backgroud
 function criarBG(){
     //cor do backgroud
     context.fillStyle = '#191970';
     // desenhando as dimenssões do jogo   
-    context.fillRect(0, 0, 16*box, 16*box);
+    context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 //trabalhando com array e tamanho 
@@ -29,7 +40,12 @@ function criarCobrinha(){
         context.fillStyle = "ForestGreen";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+}
 
+//criando a comida da cobrinha
+function drawFood(){
+    context.fillStyle = "#8B0000";
+    context.fillRect(food.x, food.y, box, box);
 }
 
 //evento do botão ao clicar
@@ -37,12 +53,16 @@ document.addEventListener('keydown', update);
 
 //criando a função do evento do botão
 function update(event){
+
     //o 37 indica a direção que é direita
     if(event.keyCode == 37 && direction != 'right') direction = 'left';
+
     //o 38 indica a direção que é para baixo
     if(event.keyCode == 38 && direction != 'down') direction = 'up';
+
     //o 39 indica a direção que é esquerda
     if(event.keyCode == 39 && direction != 'left') direction = 'right';
+
     //o 40 indica a direção que é para cima
     if(event.keyCode == 40 && direction != 'up') direction = 'down';
 }
@@ -51,7 +71,8 @@ function update(event){
 function iniciarJogo(){
     criarBG();
     criarCobrinha();
-
+    drawFood();
+    
     // redirecionanddo o movimento da cobrinha para que passa por traz da tela
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
